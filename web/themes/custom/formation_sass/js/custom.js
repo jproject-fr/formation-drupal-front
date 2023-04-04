3776064bca7215532b475abf6ca15df89f1b4a29
Jeremy THEBAULT <j.thebault@techfirm.ch>
2023-04-04 06:13:36 +0200

    SASS theme

---
+++ web/themes/custom/formation_sass/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.formation_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
